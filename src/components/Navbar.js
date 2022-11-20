747d7cae8cdbc1eb99d44766cd558fd9a926f329
Adan Abbi <adanweli@hotmail.com>
2022-11-20 02:56:16 -0800

    updated logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import { FaBars } from 'react-icons/fa'
-import logo from '../assets/logo.svg'
+
 import Links from '../constants/links'
 import SocialLinks from '../constants/socialLinks'
 const Navbar = ({ toggle }) => {
@@ -10,7 +10,7 @@ const Navbar = ({ toggle }) => {
       <div className="nav-center">
         <div className="nav-header">
           <Link to="/" className="nav-logo">
-            <img src={logo} alt="mdx logo" />
+            <h1 className="logo">AdansBlog</h1>
           </Link>
           <button className="toggle-btn" onClick={toggle}>
             <FaBars />
